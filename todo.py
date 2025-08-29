8a021252108a7acf10cba7b718f1949086aea24f
savannah567 <savannah_meyers23@yahoo.com>
2025-08-29 12:09:29 -0500

    Create todo.py

---
+++ todo.py
@@ -0,0 +1,37 @@
+# todo.py
+
+# Step 1: Start with an empty list to hold tasks
+tasks = []
+
+# Step 2: Add a task
+def add_task(task):
+    tasks.append(task)
+
+# Step 3:Print / View tasks
+def view_tasks():
+    for i, task in enumerate(tasks, start=1):
+        print(f"{i}. {task}")
+
+# Step 4: Delete a task
+def delete_task(index):
+    if 0 <= index < len(tasks):
+        tasks.pop(index)
+    else:
+        print("Invalid task number.")
+
+# Step 5: Mark task complete
+#def mark_complete():
+
+# Step 6: Save/load tasks (extra stretch for today)
+#def save_tasks():
+    
+
+# Demo flow (you can run this file directly: python todo.py)
+if __name__ == "__main__":
+    add_task("Finish Cyber 201 assignment")
+    add_task("Push code to GitHub")
+    view_tasks()
+    delete_task(tasks, 1)
+    #mark_complete(0)
+    #view_tasks()
+    #save_tasks()
